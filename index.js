f2b09d5b8fa15e503a57be9322086b8def4fbde4
reswanson <swansonr3@csp.edu>
2020-04-28 11:30:43 -0500

    fixing env var name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./models/Product');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://justtryitout:27017/node-react-starter`);
+mongoose.connect(process.env.MONGOLAB_URI || `mongodb://localhost:27017/node-react-starter`);
 
 app.use(bodyParser.json());
 
